bf7788cb6ca01875d22cd2c540c00802b1459524
JackChance <1769264507@qq.com>
2018-07-19 17:24:07 +0800

    用户删除UI

--- src/js/components/pc_managepanel_users.js
+++ src/js/components/pc_managepanel_users.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Input,Row,Col ,message,Spin,Calendar,Form,Button} from 'antd';
+import { Input,Row,Col ,message,Spin,Calendar,Form,Button,Divider,Dropdown,Menu,Icon} from 'antd';
+import { List, Avatar } from 'antd';
+
 
 const FormItem = Form.Item;
 var userList = [];
@@ -45,10 +47,12 @@ class ManagePanel_users extends React.Component {
         });
     }
 
-    deleteClicked(e) {
-        e.preventDefault();
+    deleteClicked(e, user_id) {
+        message.info('删除用户成功');
+        let {getFieldProps} = this.props.form;
 
-        const myRequest = new Request('/user/<' + e.key + '>/delete',
+
+        const myRequest = new Request('/user/<' + {user_id} + '>/delete',
                                       {method: 'GET',
                                        headers: new Headers({"Content-Type":"application/json"})});
         fetch(myRequest)
@@ -79,18 +83,59 @@ class ManagePanel_users extends React.Component {
 
     render() {
         let {getFieldProps} = this.props.form;
+        var temp = {};
+        temp.user_id = '123456';
+        temp.user_name = 'Jack';
+        temp.user_intro = '这是用户';
+        temp.user_credit = 10;
+        userList.push(temp);
+        userList.push(temp);
+        userList.push(temp);
 
         console.log('管理员界面');
         return (
             <div>
                 <Row>
-                    <Col span={7}></Col>
-                    <Col span={12}>
+                    <Divider>用户管理</Divider>
+                    <Col span={4}></Col>
+                    <Col span={16}>
                         <div>
-                            {userList}
+                        <List
+                          itemLayout="horizontal"
+                          dataSource={userList}
+                          renderItem={item => (
+                            <List.Item>
+                              <List.Item.Meta
+                                avatar={<Avatar style={{ backgroundColor: '#1E90FF', verticalAlign: 'middle' }} size="large">
+                      					         {item.user_name}</Avatar>}
+                                title={item.user_name}
+                                description={item.user_intro}
+                              />
+                              <div>信誉度：{item.user_credit}</div>
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                              <div>
+                                <div>
+                                  <Dropdown overlay={ (
+                                    <Menu>
+                                      <Menu.Item key = 'delete_user' onClick={(e,user_id) => {this.deleteClicked(e, item.user_id)}}>删除用户</Menu.Item>
+                                    </Menu>)}>
+                                        <Button style={{ marginTop: 3,marginLeft:8 }}>
+                                          用户管理<Icon type="down" />
+                                        </Button>
+                                  </Dropdown>
+                                </div>
+                              </div>
+
+                            </List.Item>
+                          )}/>
                         </div>
                     </Col>
-                    <Col span={6}></Col>
+                    <Col span={4}></Col>
                 </Row>
             </div>
         );
